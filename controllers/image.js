2ef63d25f8bea6c354c1db47efee80fdb282b696
j-ai <jvoltci@gmail.com>
2019-01-03 19:07:41 +0530

    Almost done

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+ apiKey: 'ac25fcfd8bea44e3b7d2777ee0864800'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        req.body.input)
+      .then(data => res.json(data))
+      .catch(err => res.status(400).json('unable to work with api'));
+}
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
+  })
+  .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage,
+	handleApiCall
+}
